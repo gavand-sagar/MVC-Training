ecf2436f9f8e46fcdb4d9ba37a10427e5df65b9b
gavand-sagar <gavand.sagar.s@gmail.com>
2022-05-01 05:58:39 +0530

    temp

--- Lab1/ViewModels/UserListViewModel.cs
+++ Lab1/ViewModels/UserListViewModel.cs
@@ -7,10 +7,6 @@
        public UserViewModel user = new UserViewModel();
 
         public string CurrentUser { get; set; }
-        
-        
-        //public string name { get; set; }
-        //public string age { get; set; }
-        //public string city { get; set; }
+
     }
 }
